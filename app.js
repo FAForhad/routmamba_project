d68b89a89df95a948fe5f9f01ab7a895dc91f5bf
FAForhad <faforhad450@gmail.com>
2023-03-31 20:14:18 +0600

    nav_footer_show_with_routemamba

--- app.js
+++ app.js
@@ -11,7 +11,50 @@ routemamba.register_http_routes([
     preloader: "loading...",
     error_content: "error",
     http_url_change: false,
+    http_url: "",
+  },
+  {
+    method: "GET",
+    meta_loader: true,
+    content_url: "components/home.html",
+    container: "#outlet",
+    preloader: "loading...",
+    error_content: "error",
+    http_url_change: false,
     http_url: "index.html",
+  },
+]);
+
+routemamba.register_routes_headers([
+  {
+    content_url: "components/header.html",
+    container: "#header",
+    preloader: "loading...",
+    error_content: "error",
+    http_url: [""],
+  },
+  {
+    content_url: "components/header.html",
+    container: "#header",
+    preloader: "loading...",
+    error_content: "error",
+    http_url: ["index.html"],
+  },
+]);
+routemamba.register_routes_footers([
+  {
+    content_url: "components/footer.html",
+    container: "#footer",
+    preloader: "loading...",
+    error_content: "error",
+    http_url: [""],
+  },
+  {
+    content_url: "components/footer.html",
+    container: "#footer",
+    preloader: "loading...",
+    error_content: "error",
+    http_url: ["index.html"],
   },
 ]);
 
