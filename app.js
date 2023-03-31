52865d9c5f2c7193fd080735a9ab64765a4c0cca
FAForhad <faforhad450@gmail.com>
2023-03-31 22:53:24 +0600

    routes_added

--- app.js
+++ app.js
@@ -16,6 +16,46 @@ routemamba.register_http_routes([
   {
     method: "GET",
     meta_loader: true,
+    content_url: "components/about.html",
+    container: "#outlet",
+    preloader: "loading...",
+    error_content: "error",
+    http_url_change: false,
+    http_url: "about.html",
+  },
+  {
+    method: "GET",
+    meta_loader: true,
+    content_url: "components/contact.html",
+    container: "#outlet",
+    preloader: "loading...",
+    error_content: "error",
+    http_url_change: false,
+    http_url: "contact.html",
+  },
+  {
+    method: "GET",
+    meta_loader: true,
+    content_url: "components/cycle.html",
+    container: "#outlet",
+    preloader: "loading...",
+    error_content: "error",
+    http_url_change: false,
+    http_url: "cycle.html",
+  },
+  {
+    method: "GET",
+    meta_loader: true,
+    content_url: "components/news.html",
+    container: "#outlet",
+    preloader: "loading...",
+    error_content: "error",
+    http_url_change: false,
+    http_url: "news.html",
+  },
+  {
+    method: "GET",
+    meta_loader: true,
     content_url: "components/home.html",
     container: "#outlet",
     preloader: "loading...",
@@ -24,7 +64,7 @@ routemamba.register_http_routes([
     http_url: "index.html",
   },
 ]);
-
+// header
 routemamba.register_routes_headers([
   {
     content_url: "components/header.html",
@@ -40,7 +80,37 @@ routemamba.register_routes_headers([
     error_content: "error",
     http_url: ["index.html"],
   },
+  {
+    content_url: "components/header.html",
+    container: "#header",
+    preloader: "loading...",
+    error_content: "error",
+    http_url: ["about.html"],
+  },
+  {
+    content_url: "components/header.html",
+    container: "#header",
+    preloader: "loading...",
+    error_content: "error",
+    http_url: ["cycle.html"],
+  },
+  {
+    content_url: "components/header.html",
+    container: "#header",
+    preloader: "loading...",
+    error_content: "error",
+    http_url: ["contact.html"],
+  },
+  {
+    content_url: "components/header.html",
+    container: "#header",
+    preloader: "loading...",
+    error_content: "error",
+    http_url: ["news.html"],
+  },
 ]);
+
+// footer
 routemamba.register_routes_footers([
   {
     content_url: "components/footer.html",
@@ -55,6 +125,34 @@ routemamba.register_routes_footers([
     preloader: "loading...",
     error_content: "error",
     http_url: ["index.html"],
+  },
+  {
+    content_url: "components/footer.html",
+    container: "#footer",
+    preloader: "loading...",
+    error_content: "error",
+    http_url: ["about.html"],
+  },
+  {
+    content_url: "components/footer.html",
+    container: "#footer",
+    preloader: "loading...",
+    error_content: "error",
+    http_url: ["contact.html"],
+  },
+  {
+    content_url: "components/footer.html",
+    container: "#footer",
+    preloader: "loading...",
+    error_content: "error",
+    http_url: ["cycle.html"],
+  },
+  {
+    content_url: "components/footer.html",
+    container: "#footer",
+    preloader: "loading...",
+    error_content: "error",
+    http_url: ["news.html"],
   },
 ]);
 
